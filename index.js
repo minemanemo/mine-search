cd4a3e5320bbcfafa5a5627323817efebcffe328
minhoe <minhoe@netman.co.kr>
2020-07-29 12:46:27 +0900

    READMD.md 추가

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function generateTwoDemension(n = 10) {
 }
 
    // {size}개의 random mine 좌표 (기본 10개)
-function generateMineXY(size = 10) {
+function generateMineXY(size = 10, num = 10) {
     const list = [];
 
-    while (list.length < size) {
+    while (list.length < num) {
         const x = Math.floor(Math.random() * size);
         const y = Math.floor(Math.random() * size);
 
@@ -44,6 +44,7 @@ function getMineMap() {
             const newX = x + xy[0];
             const newY = y + xy[1];
 
+            // Map을 넘어가거나 지뢰인 경우 카운팅 제외
             newX >= 0 && newX < 10
                 && newY >= 0 && newY < 10
                 && book[newX][newY] !== '*' && book[newX][newY]++;
